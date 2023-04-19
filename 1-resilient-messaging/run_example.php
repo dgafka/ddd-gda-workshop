c9c6fc69075a3d16f4889e29ac39fbc095e29971
Dariusz Gafka <dariuszgafka@gmail.com>
2023-04-19 18:16:41 +0200

    more details

--- 1-resilient-messaging/run_example.php
+++ 1-resilient-messaging/run_example.php
@@ -3,6 +3,7 @@
 use App\Application\PlaceOrder;
 use App\Domain\ShippingService;
 use App\Infrastructure\NetworkFailingShippingService;
+use Ecotone\Dbal\Recoverability\DeadLetterGateway;
 use Ecotone\Lite\EcotoneLiteApplication;
 use Ecotone\Messaging\Config\ServiceConfiguration;
 use Ecotone\Messaging\Endpoint\ExecutionPollingMetadata;
@@ -13,36 +14,45 @@ use Ramsey\Uuid\Uuid;
 
 require __DIR__ . "/vendor/autoload.php";
 
-$ecotoneLite = EcotoneLiteApplication::bootstrap([ShippingService::class => new NetworkFailingShippingService(), DbalConnectionFactory::class => new DbalConnectionFactory(getenv('DATABASE_DSN') ? getenv('DATABASE_DSN') : 'pgsql://ecotone:secret@localhost:5432/ecotone'), AmqpConnectionFactory::class => new AmqpConnectionFactory(['dsn' => getenv('RABBIT_DSN') ? getenv('RABBIT_DSN') : 'amqp://guest:guest@localhost:5672/%2f'])], serviceConfiguration: ServiceConfiguration::createWithDefaults()->withServiceName('ddd_gda_service')->withDefaultErrorChannel('errorChannel'), pathToRootCatalog: __DIR__);
-/** @var AmqpConnectionFactory $amqpConnectionFactory */
-$amqpConnectionFactory = $ecotoneLite->getServiceFromContainer(AmqpConnectionFactory::class);
-$amqpConnectionFactory->createContext()->deleteQueue(new \Interop\Amqp\Impl\AmqpQueue('orders'));
-$executionPollingMetadata = ExecutionPollingMetadata::createWithDefaults()->withExecutionTimeLimitInMilliseconds(2000)->withHandledMessageLimit(1);
+$serviceName = 'ddd_gda_service';
+$shippingService = new NetworkFailingShippingService();
+$ecotoneLite = EcotoneLiteApplication::bootstrap([ShippingService::class => $shippingService, NetworkFailingShippingService::class => $shippingService, DbalConnectionFactory::class => new DbalConnectionFactory(getenv('DATABASE_DSN') ? getenv('DATABASE_DSN') : 'pgsql://ecotone:secret@localhost:5432/ecotone'), AmqpConnectionFactory::class => new AmqpConnectionFactory(['dsn' => getenv('RABBIT_DSN') ? getenv('RABBIT_DSN') : 'amqp://guest:guest@localhost:5672/%2f'])], serviceConfiguration: ServiceConfiguration::createWithDefaults()->withServiceName($serviceName)->withDefaultErrorChannel('errorChannel'), pathToRootCatalog: __DIR__);
+cleanup($ecotoneLite);
+$executionPollingMetadata = ExecutionPollingMetadata::createWithDefaults()->withExecutionTimeLimitInMilliseconds(1000)->withHandledMessageLimit(1);
 
-$commandBus = $ecotoneLite->getCommandBus();
-$queryBus = $ecotoneLite->getQueryBus();
+echo "\n\n\nSkładamy zamówienia na Laptopa\n";
+$ecotoneLite->getCommandBus()->send(new PlaceOrder(Uuid::uuid4()->toString(), "Laptop"));
 
-echo "Składamy zamówienia na Laptopa\n";
-$commandBus->send(new PlaceOrder(Uuid::uuid4()->toString(), "Laptop"));
-
-echo "Oczekuje na asynchroniczną wiadomość. Błąd przetwarzania wiadomości wystąpi cztery razy.\n";
+echo "Ecotone stworzy dla Ciebie kolejkę w RabbitMQ (http://localhost:15672/#/queues) oraz serializuje/odserializuje wiadomości. Błąd przetwarzania wiadomości wystąpi cztery razy.\n\n";
+echo "Oczekuje na asynchroniczną wiadomość.\n";
 $ecotoneLite->run("orders", $executionPollingMetadata);
-echo "Ecotone stworzył dla Ciebie kolejkę w RabbitMQ (http://localhost:15672/#/queues) oraz serializował/odserializował wiadomość. Pierwsze przetworzenie wiadomości zakończone, jesteśmy coraz blizej!\n";
+echo "Pierwsze przetworzenie wiadomości zakończone, jesteśmy coraz blizej!\n\n";
 
 echo "Ponawiam wiadomość. Drugie przetworzenie wiadomości.\n";
 sleep(2);
 $ecotoneLite->run('orders', $executionPollingMetadata);
-echo "Ponawiam wiadomość. Trzecie przetworzenie wiadomości.\n";
+echo "\nPonawiam wiadomość. Trzecie przetworzenie wiadomości.\n";
 sleep(2);
 $ecotoneLite->run('orders', $executionPollingMetadata);
-echo "Ponawiam wiadomość. Czwarte przetworzenie wiadomości.\n";
+echo "\nPonawiam wiadomość. Czwarte przetworzenie wiadomości.\n";
 sleep(2);
 $ecotoneLite->run('orders', $executionPollingMetadata);
 
-echo "Twoja wiadomość wyczerpała liczbę ponowień i została zapisana w Dead Letter.\n";
+echo "\n\nTwoja wiadomość wyczerpała liczbę ponowień i została zapisana w Dead Letter.\n";
 echo "Ponów wiadomość bezpośrednio z Ecotone Pulse: http://localhost:3000, aby zakończyć zadanie :)\n";
 
-$ecotoneLite->run('orders', ExecutionPollingMetadata::createWithDefaults()->withExecutionTimeLimitInMilliseconds(60000)->withHandledMessageLimit(1)->withStopOnError(true));
+/** Consumer dla wiadomości rozproszonych (komunikacja z Ecotone Pulse) */
+$ecotoneLite->run($serviceName, ExecutionPollingMetadata::createWithDefaults()->withExecutionTimeLimitInMilliseconds(60000)->withHandledMessageLimit(1));
+$ecotoneLite->run('orders', $executionPollingMetadata);
 
 Assert::assertTrue($ecotoneLite->getQueryBus()->sendWithRouting("isShippingSuccessful"));
-echo "Udało się dostarczyć zamówienie do klienta. Gratulacje, zadanie ukończone!\n";
+echo "Udało się przetworzyć wiadomość i dostarczyć laptopa do klienta. Gratulacje, zadanie ukończone!\n\n";
+
+function cleanup(\Ecotone\Messaging\Config\ConfiguredMessagingSystem $ecotoneLite): void
+{
+    /** @var AmqpConnectionFactory $amqpConnectionFactory */
+    $amqpConnectionFactory = $ecotoneLite->getServiceFromContainer(AmqpConnectionFactory::class);
+    $amqpConnectionFactory->createContext()->deleteQueue(new \Interop\Amqp\Impl\AmqpQueue('orders'));
+    $amqpConnectionFactory->createContext()->deleteQueue(new \Interop\Amqp\Impl\AmqpQueue('distributed_example_service'));
+    $ecotoneLite->getGatewayByName(DeadLetterGateway::class)->deleteAll();
+}
